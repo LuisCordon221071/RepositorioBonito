e864f47c8810128a3745c093e44ba477bec53a5d
LuisCordon221071 <cor221071@uvg.edu.gt>
2024-01-21 18:25:14 -0600

    Cargar la interfaz

---
+++ Radio/src/IRadio.java
@@ -0,0 +1,17 @@
+/* Algoritmos y estructuras de datos [Sección 50]
+ * Interfaz para la radio (Propuesta)
+ */
+
+ public interface IRadio {
+
+    boolean cambioBinario(boolean valor);
+
+    int[] guardarAM(int[] lista_AM, int posicion, int estacion);
+
+    double[] guardarFM(double[] lista_FM, int posicion, double estacion);
+
+    int avanzarAM(int estacion_actual);
+
+    double avanzarFM(double estacion_actual);
+
+}
