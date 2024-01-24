d9edd46874c11544086734613c7db9bc9113b8b8
LuisCordon221071 <cor221071@uvg.edu.gt>
2024-01-24 10:58:40 -0600

    commit

---
+++ EjemploGenerics/Src/StackUsingNodes.java
@@ -0,0 +1,42 @@
+public class StackUsingNodes implements IStack {
+
+    private int size;
+    private Node head;
+
+    public StackUsingNodes(){
+        size = 0;
+        head = null;
+    }
+
+    @Override
+    public int count() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'count'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public void push(int value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'push'");
+    }
+
+    @Override
+    public int pop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pop'");
+    }
+
+    @Override
+    public int peek() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+    }
+
+    
+}
