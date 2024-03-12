d0875276a42c4ccaccbd63c4fca3418c63bb84de
LuisCordon221071 <cor221071@uvg.edu.gt>
2024-03-11 22:49:59 -0600

    Última actualización (2 CPU)

--- Simpy/SimSimpy.py
+++ Simpy/SimSimpy.py
@@ -2,13 +2,13 @@ import simpy
 import random
 
 RANDOM_SEED = 150
-CPU_INSTRUCCIONES = 3  
+CPU_INSTRUCCIONES = 3
 MEMORY_CAPACITY = 100
 NUM_PROCESOS = 200
-INTERVALO = 10 
-VELOCIDAD_PROCESADOR = 1
+INTERVALO = 5  
+NUM_CPUS = 2  
 
-def proceso(env, nombre, ram, cpu):
+def proceso(env, nombre, ram, cpus):
     memoria_necesaria = random.randint(1, 10)
     with ram.get(memoria_necesaria) as req:
         yield req
@@ -18,11 +18,10 @@ def proceso(env, nombre, ram, cpu):
         instrucciones_restantes = instrucciones_totales
         
         while instrucciones_restantes > 0:
-            with cpu.request() as req_cpu:
-                yield req_cpu
+            with cpus.request() as req_cpus:
+                yield req_cpus
                 instrucciones_a_realizar = min(instrucciones_restantes, CPU_INSTRUCCIONES)
-                tiempo_requerido = instrucciones_a_realizar / VELOCIDAD_PROCESADOR 
-                yield env.timeout(tiempo_requerido)  
+                yield env.timeout(1)  # Se ejecutan las instrucciones en 1 unidad de tiempo
                 instrucciones_restantes -= instrucciones_a_realizar
                 print(f'{nombre} ha completado {instrucciones_a_realizar} instrucciones en {env.now}')
             
@@ -43,18 +42,19 @@ def proceso(env, nombre, ram, cpu):
         print(f'{nombre} ha liberado {memoria_necesaria} de memoria RAM en {env.now}')
                 
 
-def llegada_procesos(env, ram, cpu):
+def llegada_procesos(env, ram, cpus):
     for i in range(NUM_PROCESOS):
-        env.process(proceso(env, f'Proceso-{i+1}', ram, cpu))
-        yield env.timeout(random.expovariate(1.0 / INTERVALO)) 
+        env.process(proceso(env, f'Proceso-{i+1}', ram, cpus))
+        yield env.timeout(random.expovariate(1.0 / INTERVALO))  
 
 
 random.seed(RANDOM_SEED)
 env = simpy.Environment()
 ram = simpy.Container(env, init=MEMORY_CAPACITY, capacity=MEMORY_CAPACITY)
-cpu = simpy.Resource(env, capacity=1)
-env.process(llegada_procesos(env, ram, cpu))
+cpus = simpy.Resource(env, capacity=NUM_CPUS)  
+env.process(llegada_procesos(env, ram, cpus))
 env.run(until=3000)
+
 
 
 
